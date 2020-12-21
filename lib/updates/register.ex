acc59517b4b3b2c19633d800620e9ce4965cc57e
Shinmera <shinmera@tymoon.eu>
2020-12-21 18:55:06 +0100

    Implement register update.

--- lib/updates/register.ex
+++ lib/updates/register.ex
@@ -1,7 +1,13 @@
 use Update
 defupdate(Register, "REGISTER", [:password]) do
-  def handle(_type, update, state) do
-    Connection.write(state, Update.fail(update, Update.RegistrationRejected))
+  def handle(type, update, state) do
+    case Profile.register(Profile, %Profile{name: type.from, password: update.password}) do
+      :ok ->
+        Connection.write(state, update)
+      {:error, reason} ->
+        Connection.write(state, Update.fail(update, Update.RegistrationRejected, [
+                text: reason )))
+    end
     state
   end
 end
