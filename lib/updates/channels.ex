35fae4d0a909ce1751ce375a2708f532eeda38cf
Shinmera <shinmera@tymoon.eu>
2020-12-24 17:38:42 +0100

    Filter listed channels.

--- lib/updates/channels.ex
+++ lib/updates/channels.ex
@@ -1,8 +1,8 @@
 use Update
 defupdate(Channels, "CHANNELS", []) do
   def handle(_type, update, state) do
-    ## TODO: Check permissions on each and filter.
-    Connection.write(state, Update.reply(update, Update.Channels, [channels: Channel.list(Channel)]))
+    channels = Enum.filter(Channel.list(Channel), &Channel.permitted(&1, update))
+    Connection.write(state, Update.reply(update, Update.Channels, [channels: channels]))
     state
   end
 end
