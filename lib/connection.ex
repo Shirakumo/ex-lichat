dee92bc426567bef729b5064b4b3aa2b8af0eba5
Shinmera <shinmera@tymoon.eu>
2020-12-26 15:32:20 +0100

    Handle connection chunking with null termination

--- lib/connection.ex
+++ lib/connection.ex
@@ -1,7 +1,7 @@
 defmodule Connection do
   use Task
   require Logger
-  defstruct socket: nil, user: nil, name: nil, state: nil
+  defstruct socket: nil, user: nil, name: nil, state: nil, accumulator: <<>>
 
   def start_link(socket) do
     :inet.setopts(socket, [active: true])
@@ -10,51 +10,14 @@ defmodule Connection do
   
   def run(state) do
     next_state =
-    receive do
+      receive do
       {:tcp, socket, data} ->
-        # TODO: Reconstruct full updates from partial data,
-        #       skip on length exceed or broken update, etc.
-        #       Check permissions for user
         state = %{state | socket: socket}
-        Logger.info("> #{data}")
-        try do
-          update = Update.parse(data)
-          try do
-            case state.state do
-              nil ->
-                if update.type.__struct__ == Update.Connect do
-                  Update.handle(update, state)
-                else
-                  write(state, Update.fail(Update.InvalidUpdate))
-                  close(state)
-                end
-              :closed ->
-                close(state)
-              _ ->
-                update = case update.from do
-                           nil -> %{update | from: state.name}
-                           _ -> update
-                         end
-                cond do
-                  update.from != state.name ->
-                    write(state, Update.fail(update, Update.UsernameMismatch))
-                  not Update.permitted?(update) ->
-                    write(state, Update.fail(update, Update.InsufficientPermissions))
-                  true ->
-                    Update.handle(update, state)
-                end
-            end
-          rescue
-            e in RuntimeError ->
-              write(state, Update.fail(update, Update.UpdateFailure, [text: e.message]))
-          end
-        rescue
-          e in Error.ParseFailure ->
-            write(state, Update.fail(Update.MalformedUpdate, e.message))
-          e in Error.UnsupportedUpdate ->
-            write(state, Update.fail(Update.InvalidUpdate, e.message))
-          e in RuntimeError ->
-            write(state, Update.fail(Update.Failure, e.message))
+        case stitch(state, data, Toolkit.config(:max_update_size, 8192)) do
+          {:ok, update, state} ->
+            handle_update(state, update)
+          {:more, state} ->
+            state
         end
       {:tcp_closed, _} ->
         Logger.info("TCP closed #{inspect(state.user)}")
@@ -66,6 +29,77 @@ defmodule Connection do
         write(state, msg)
     end
     run(next_state)
+  end
+
+  def stitch(state, data, max_size) do
+    case state.accumulator do
+      :dropping ->
+        case :binary.split(data, <<0>>) do
+          [ _, rest ] ->
+            stitch(%{state | accumulator: <<>>}, rest, max_size)
+          _ ->
+            {:more, state}
+        end
+      acc ->
+        data = acc <> data
+        case :erlang.decode_packet(:line, data, [line_length: max_size]) do
+          {:ok, payload, rest} ->
+            if :binary.last(payload) != 0 and byte_size(payload) == max_size do
+              write(state, Update.fail(Update.UpdateTooLong))
+              stitch(%{state | accumulator: :dropping}, rest, max_size)
+            else
+              {:ok, payload, %{state | accumulator: rest}}
+            end
+          {:more, _} ->
+            {:more, %{state | accumulator: data}}
+          ## This case should never occur, but we handle it anyway.
+          {:error, reason} ->
+            write(state, Update.fail(Update.MalformedUpdate, [text: reason]))
+            {:more, %{state | accumulator: :dropping}}
+        end
+    end
+  end
+
+  def handle_update(state, data) do
+    try do
+      update = Update.parse(data)
+      try do
+        case state.state do
+          nil ->
+            if update.type.__struct__ == Update.Connect do
+              Update.handle(update, state)
+            else
+              write(state, Update.fail(Update.InvalidUpdate))
+              close(state)
+            end
+          :closed ->
+            close(state)
+          _ ->
+            update = case update.from do
+                       nil -> %{update | from: state.name}
+                       _ -> update
+                     end
+            cond do
+              update.from != state.name ->
+                write(state, Update.fail(update, Update.UsernameMismatch))
+              not Update.permitted?(update) ->
+                write(state, Update.fail(update, Update.InsufficientPermissions))
+              true ->
+                Update.handle(update, state)
+            end
+        end
+      rescue
+        e in RuntimeError ->
+          write(state, Update.fail(update, Update.UpdateFailure, [text: e.message]))
+      end
+    rescue
+      e in Error.ParseFailure ->
+        write(state, Update.fail(Update.MalformedUpdate, e.message))
+      e in Error.UnsupportedUpdate ->
+        write(state, Update.fail(Update.InvalidUpdate, e.message))
+      e in RuntimeError ->
+        write(state, Update.fail(Update.Failure, e.message))
+    end
   end
 
   def write(state, update) do
