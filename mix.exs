e57aeba4fe48a43aeb24f787b597052b3b9c3e8a
Shinmera <shinmera@tymoon.eu>
2020-12-27 23:48:49 +0100

    Configure release.

--- mix.exs
+++ mix.exs
@@ -7,7 +7,15 @@ defmodule Lichat.MixProject do
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      releases: [
+        lichat: [
+          include_executables_for: [:unix],
+          steps: [:assemble, :tar],
+          applications: [lichat: :permanent],
+          version: {:from_app, :lichat}
+        ]
+      ]
     ]
   end
 
