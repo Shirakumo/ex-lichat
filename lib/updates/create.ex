c254d261a217be380d615f8904b3b2903177e05e
Shinmera <shinmera@tymoon.eu>
2020-12-28 10:22:52 +0100

    Check channel names

--- lib/updates/create.ex
+++ lib/updates/create.ex
@@ -1,18 +1,20 @@
 use Update
 defupdate(Create, "CREATE", [:channel]) do
   def handle(type, update, state) do
-    case type.channel do
-      nil ->
+    cond do
+      type.channel in [false, nil, ""] ->
         {name, channel} = Channel.make(Channel)
         User.join(state.user, channel)
         Connection.write(state, Update.reply(update, Update.Join, [channel: name]))
-      name ->
-        case Channel.ensure_channel(Channel, name, update.from) do
+      not Toolkit.valid_name?(type.channel) ->
+        Connection.write(state, Update.fail(update, Update.BadName))
+      true ->
+        case Channel.ensure_channel(Channel, type.channel, update.from) do
           {:old, _} ->
             Connection.write(state, Update.fail(update, Update.ChannelnameTaken))
           {:new, channel} ->
             User.join(state.user, channel)
-            Connection.write(state, Update.reply(update, Update.Join, [channel: name]))
+            Connection.write(state, Update.reply(update, Update.Join, [channel: type.channel]))
         end
     end
     state
