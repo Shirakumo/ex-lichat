21e78598353b02e5d2d4e44a23a0d43eae3df11c
Shinmera <shinmera@tymoon.eu>
2020-12-28 10:16:56 +0100

    Fix tests

--- test/update_test.exs
+++ test/update_test.exs
@@ -4,7 +4,7 @@ defmodule UpdateTest do
   
   test "parse" do
     assert_raise Error.ParseFailure, fn->Update.parse("()") end
-    assert_raise RuntimeError, fn->Update.parse("(ping)") end
+    assert_raise Error.ParseFailure, fn->Update.parse("(ping)") end
     assert %Update{id: 0, clock: 0, from: "", type: %Update.Ping{}}
     == Update.parse("(ping :id 0 :clock 0 :from \"\")")
     assert %Update{id: 0, clock: 0, from: "", type: %Update.Message{channel: "a", text: "b"}}
@@ -12,9 +12,9 @@ defmodule UpdateTest do
   end
 
   test "print" do
-    assert "(ping :id 0 :clock 0 :from \"\")\0"
+    assert "(ping :id 0 :clock 0 :from \"\")\n\0"
     == Update.print(%Update{id: 0, clock: 0, from: "", type: %Update.Ping{}})
-    assert "(message :id 0 :clock 0 :from \"\" :channel \"a\" :text \"b\")\0"
+    assert "(message :id 0 :clock 0 :from \"\" :channel \"a\" :text \"b\")\n\0"
     == Update.print(%Update{id: 0, clock: 0, from: "", type: %Update.Message{channel: "a", text: "b"}})
   end
 end
