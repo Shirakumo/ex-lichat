abcefdb054410044476fe971f0d9c1a7aa8cf8ce
Shinmera <shinmera@tymoon.eu>
2021-04-07 08:39:51 +0200

    Fix pull update handling

--- lib/updates/pull.ex
+++ lib/updates/pull.ex
@@ -1,21 +1,26 @@
 use Update
 defupdate(Pull, "PULL", [:channel, :target]) do
   def handle(type, update, state) do
-    case Channel.get(type.channel) do
-      {:ok, channel} ->
-        cond do
-          not User.in_channel?(state.user, channel) ->
-            Connection.write(state, Update.fail(update, Update.NotInChannel))
-          User.in_channel?(type.target, channel) ->
-            Connection.write(state, Update.fail(update, Update.AlreadyInChannel))
-          true ->
-            User.join(type.target, channel)
-            Channel.write(channel, Update.reply(update, Update.Join, [
-                      from: type.target,
-                      channel: type.channel]))
+    case User.get(type.target) do
+      {:ok, target} ->
+        case Channel.get(type.channel) do
+          {:ok, channel} ->
+            cond do
+              not User.in_channel?(state.user, channel) ->
+                Connection.write(state, Update.fail(update, Update.NotInChannel))
+              User.in_channel?(target, channel) ->
+                Connection.write(state, Update.fail(update, Update.AlreadyInChannel))
+              true ->
+                User.join(target, channel)
+                Channel.write(channel, Update.reply(update, Update.Join, [
+                          from: type.target,
+                          channel: type.channel]))
+            end
+          :error ->
+            Connection.write(state, Update.fail(update, Update.NoSuchChannel))
         end
       :error ->
-        Connection.write(state, Update.fail(update, Update.NoSuchChannel))
+        Connection.write(state, Update.fail(update, Update.NoSuchUser))
     end
     state
   end
