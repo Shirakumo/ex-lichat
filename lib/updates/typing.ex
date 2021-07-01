67d0d13e5814af927c9bc84709623c04adc57617
Shinmera <shinmera@tymoon.eu>
2021-07-01 22:39:35 +0200

    Typing extension support.

---
+++ lib/updates/typing.ex
@@ -0,0 +1,19 @@
+use Update
+defupdate(Typing, "TYPING", [:channel, [:bridge, optional: true]]) do
+  def handle(type, update, state) do
+    case Channel.get(type.channel) do
+      {:ok, channel} ->
+        cond do
+          not User.in_channel?(state.user, channel) ->
+            Lichat.Connection.write(state, Update.fail(update, Update.NotInChannel))
+          is_binary(type.bridge) ->
+            Update.Bridge.bridge(type, update, state, channel)
+          true->
+            Channel.write(channel, %{update | type: Map.delete(type, :bridge)})
+        end
+      :error ->
+        Lichat.Connection.write(state, Update.fail(update, Update.NoSuchChannel))
+    end
+    state
+  end
+end
