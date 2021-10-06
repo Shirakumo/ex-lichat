4c17bbfaf9ce61d89af7d24595bec2b970c6ad13
Shinmera <shinmera@tymoon.eu>
2021-10-06 18:27:12 +0200

    Automatically update clients with new emotes when reloading

--- lib/emote.ex
+++ lib/emote.ex
@@ -12,13 +12,32 @@ defmodule Emote do
     end
   end
 
-  def reload(server) do
+  def reload(server), do: reload(server, false)
+  def reload(server, notify) do
     Logger.info("Reloading emotes")
     dir = Toolkit.config(:emote_directory)
     case File.ls(dir) do
-      {:ok, files} -> 
+      {:ok, files} ->
+        old = case Agent.get(server, &(&1)) do
+                :error -> %{}
+                emotes -> emotes
+              end
         emotes = Map.new(Enum.reject(Enum.map(files, &load_emote(dir <> &1)), &(&1 == nil)), &{&1.name, &1})
         Agent.update(server, fn _ -> emotes end)
+        if notify do
+          users = User.list(:pids)
+          Enum.each(emotes, fn {name, emote} ->
+            if not Map.has_key?(old, name) do
+              Enum.each(users, fn {_name, user} ->
+                User.write(user, Update.make(Update.Emote, [
+                      from: Lichat.server_name(),
+                      name: emote.name,
+                      content_type: emote.type,
+                      payload: emote.payload ]))
+              end)
+            end
+          end)
+        end
         emotes
       {:error, reason} ->
         error = :file.format_error(reason)
