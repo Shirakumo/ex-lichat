5d6e3e40dca4ce620cf409cb1ffaaa6242a5ad1e
Shinmera <shinmera@tymoon.eu>
2022-01-02 14:38:57 +0100

    Fix channels listing

--- lib/updates/channels.ex
+++ lib/updates/channels.ex
@@ -1,5 +1,5 @@
 use Update
-defupdate(Channels, "CHANNELS", [[:channel, required: false]]) do
+defupdate(Channels, "CHANNELS", [[:channels, required: false], [:channel, required: false]]) do
   require Logger 
   def handle(type, update, state) do
     channels = Channel.list(type.channel, :pids)
