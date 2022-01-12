cdc95a86b490510db0d81c1d822271a72f0c1c3c
Shinmera <shinmera@tymoon.eu>
2022-01-12 20:21:47 +0100

    Fix emotes update to allow optional names field.

--- lib/updates/emotes.ex
+++ lib/updates/emotes.ex
@@ -1,18 +1,19 @@
 use Update
-defupdate(Emotes, "EMOTES", [:names, [:channel, optional: true]]) do
+defupdate(Emotes, "EMOTES", [[:names, optional: true], [:channel, optional: true]]) do
   def handle(type, update, state) do
     channelname = if is_nil(type.channel), do: Lichat.server_name(), else: type.channel
     case Channel.get(channelname) do
       {:ok, channel} ->
         if User.in_channel?(state.user, channel) do
-              Enum.each(Emote.list(channelname, type.names), fn emote ->
-                Lichat.Connection.write(state, Update.reply(update, Update.Emote, [
-                          from: Lichat.server_name(),
-                          channel: channelname,
-                          name: emote.name,
+          names = if is_list(type.names), do: type.names, else: []
+          Enum.each(Emote.list(channelname, names), fn emote ->
+            Lichat.Connection.write(state, Update.reply(update, Update.Emote, [
+                      from: Lichat.server_name(),
+                      channel: channelname,
+                      name: emote.name,
                           content_type: emote.type,
-                          payload: emote.payload ]))
-              end)
+                      payload: emote.payload ]))
+          end)
         else
           Lichat.Connection.write(state, Update.fail(update, Update.NotInChannel))
         end
