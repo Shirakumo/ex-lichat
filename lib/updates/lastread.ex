c187794d42bb36bc6a25fb3e11793207360c61f0
Shinmera <shinmera@tymoon.eu>
2022-02-14 15:30:07 +0100

    make sure to check last-read for existing profile.

--- lib/updates/lastread.ex
+++ lib/updates/lastread.ex
@@ -3,12 +3,17 @@ defupdate(LastRead, "LAST-READ", [:channel, [:target, optional: true], [:update_
   def handle(type, update, state) do
     case Channel.get(type.channel) do
       {:ok, channel} ->
-        if type.target != nil and type.update_id != nil do
-          Channel.last_read(channel, update.from, type.target, type.update_id)
-          User.write(state.user, update)
-        else
-          {target, id} = Channel.last_read(channel, update.from)
-          Lichat.Connection.write(state, %{update | type: %{type | target: target, update_id: id}})
+        case Profile.lookup(update.from) do
+          :ok ->
+            if type.target != nil and type.update_id != nil do
+              Channel.last_read(channel, update.from, type.target, type.update_id)
+              User.write(state.user, update)
+            else
+              {target, id} = Channel.last_read(channel, update.from)
+              Lichat.Connection.write(state, %{update | type: %{type | target: target, update_id: id}})
+            end
+          :not_registered ->
+            Lichat.Connection.write(state, Update.fail(update, Update.NoSuchProfile))
         end
       :error ->
         Lichat.Connection.write(state, Update.fail(update, Update.NoSuchChannel))
