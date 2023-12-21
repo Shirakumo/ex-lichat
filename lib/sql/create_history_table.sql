f504db38f7d8cb98664f7ff3e7e8ee67a19030d7
Shinmera <shinmera@tymoon.eu>
2023-12-21 14:03:03 +0100

    Add indices to history

--- lib/sql/create_history_table.sql
+++ lib/sql/create_history_table.sql
@@ -9,3 +9,5 @@ CREATE TABLE IF NOT EXISTS "lichat-history"(
        "markup" VARCHAR(16),
        CONSTRAINT "channel" FOREIGN KEY("channel") REFERENCES "lichat-history-channels"("id") ON DELETE CASCADE
 );
+CREATE INDEX IF NOT EXISTS "lichat-history.channel" ON "lichat-history" USING hash("channel");
+CREATE INDEX IF NOT EXISTS "lichat-history.clock" ON "lichat-history" USING btree("clock") DESC;
